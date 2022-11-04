7df65eb4bd29ae31e5a4e3d50408ab7bb12e5dd3
rosi <rosi>
2022-11-04 17:52:00 +0000

    peer review branch

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -17,3 +17,53 @@ def valid?(password)
         return false
     end
 end
+
+def valid_length?(password)
+    password.length >= 8
+end 
+
+def has_special_char?(password)
+    if password.include?('!')
+        return true
+    elsif password.include?('@')
+        return true
+    elsif password.include?('$')
+        return true
+    elsif password.include?('%')
+        return true
+    elsif password.include?('&')
+        return true 
+    else 
+        return false
+    end
+end
+
+def valid?(password)
+    if valid_length?(password) && has_special_char?(password)
+        return true
+    else 
+        return false
+    end
+end
+
+def valid_weak?(password)
+    if valid_length?(password) || has_special_char?(password)
+        return true
+    else
+        return false
+    end
+end
+
+REQUIRE_CHARS = /[!@$%&]/
+
+def has_special_char?(password)
+    if password =~ REQUIRE_CHARS
+        return true
+    else
+        return false
+    end
+end
+
+puts valid?('12345678!')
+
+#hello
