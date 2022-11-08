01bd63c71389dbd2d29418589e7714e7ac8d27e8
rosi <rosi>
2022-11-08 15:13:35 +0000

    peer_reviewed

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,68 +3,11 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
-    if password.length >= 8 && password.include?('!')
-        return true
-    elsif password.length >= 8 && password.include?('@')
-        return true
-    elsif password.length >= 8 && password.include?('$')
-        return true
-    elsif password.length >= 8 && password.include?('%')
-        return true
-    elsif password.length >= 8 && password.include?('&')
-        return true
-    else 
-        return false
-    end
-end
-
-def valid_length?(password)
-    password.length >= 8
-end 
-
-def has_special_char?(password)
-    if password.include?('!')
-        return true
-    elsif password.include?('@')
-        return true
-    elsif password.include?('$')
-        return true
-    elsif password.include?('%')
-        return true
-    elsif password.include?('&')
-        return true 
-    else 
-        return false
-    end
-end
-
-def valid?(password)
-    if valid_length?(password) && has_special_char?(password)
-        return true
-    else 
-        return false
-    end
-end
-
-def valid_weak?(password)
-    if valid_length?(password) || has_special_char?(password)
+    special = "!@$%&"
+    regex = /[#special.gsub(/./){|char| "\\#{char}"}}]/
+    if password.length > 7 && password =~ regex
         return true
     else
         return false
     end
 end
-
-REQUIRE_CHARS = /[!@$%&]/
-
-def has_special_char?(password)
-    if password =~ REQUIRE_CHARS
-        return true
-    else
-        return false
-    end
-end
-
-puts valid?('12345678!')
-
-#hello
-#hello 2
